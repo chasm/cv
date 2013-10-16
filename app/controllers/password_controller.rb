572136ca54edc0fc1e4f015797119d1ac608d7c8
chasm <charles.munat@gmail.com>
2013-10-16 16:42:13 +0800

    Added password#update action.

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -13,7 +13,22 @@ class PasswordController < ApplicationController
   end
 
   def update
+    @user = User.find_by(code: params[:code])
     
+    if @user and @user.expires_at > Time.now
+      @user.update_attributes(user_params)
+      @user.code = nil
+      @user.expires_at = nil
+      
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_url, notice: "Password changed successfully."
+      else
+        redirect_to login_url, alert: "Reset link has expired. Please try again."
+      end
+    else
+      redirect_to login_url, alert: "Reset link has expired. Please try again."
+    end
   end
   
   private
