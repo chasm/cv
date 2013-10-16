800f591ddd23263cbcb29fcda6eb3660f794f11d
chasm <charles.munat@gmail.com>
2013-10-16 16:27:25 +0800

    Completed password#edit action.

--- app/controllers/password_controller.rb
+++ app/controllers/password_controller.rb
@@ -1,7 +1,15 @@
 class PasswordController < ApplicationController
   
   def edit
+    @title = "Reset Password"
     
+    @user = User.find_by(code: params[:code])
+    
+    if @user and @user.expires_at > Time.now
+      render :edit
+    else
+      redirect_to login_url, alert: "Reset link has expired. Please try again."
+    end
   end
 
   def update
