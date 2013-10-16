fdbdfa1af57ab81551ef3479947dd4a5a6bf004d
chasm <charles.munat@gmail.com>
2013-10-16 17:09:48 +0800

    Added get_user method to UserController.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,8 @@
 class UserController < ApplicationController
   before_action :is_authenticated
+  before_action :get_user, except: [ :new, :create ]
   
   def show
-    @user = current_user
   end
   
   def new
@@ -14,18 +14,19 @@ class UserController < ApplicationController
   end
   
   def edit
-    
   end
   
   def update
-    
   end
   
   def destroy
-    
   end
   
   private
+  
+  def get_user
+    @user = current_user
+  end
   
   def user_params
     params.require(:user).permit(
