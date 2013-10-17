75d948cc62df921d500b4cc981218b0da3c7c959
chasm <charles.munat@gmail.com>
2013-10-17 20:45:19 +0800

    Added Bootstrap modal for account deletion confirmation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,53 @@
 //= require ./vendor/numeral
 //= require ./vendor/moment
 //= require ./vendor/datetimepicker
+
+$(function() {
+  $('a.delete-link').click(function(event) {
+    $('#delete-warning').html('Are you certain that you wish to delete your account? This action cannot be undone.');
+    $('#delete-modal').modal('show');
+
+    return false;
+  });
+  
+  $.rails.confirmed = function(link) {
+    link.removeAttr('data-confirm');
+    link.trigger('click.rails');
+  };
+  
+  $.rails.showConfirmDialog = function(link){
+    var html = 
+      '<div class="modal" id="confirmationDialog" role="dialog" aria-labelledby="delete-model-title" aria-hidden="true">' +
+        '<div class="modal-dialog">' +
+          '<div class="modal-content">' +
+            '<div class="modal-header">' +
+              '<a class="close" data-dismiss="modal">&times;</a>' +
+              '<h3 id="delete-modal-title">Please confirm your account deletion</h3>' +
+            '</div>' +
+            '<div class="modal-body">' +
+              '<p>Are you certain that you want to delete your account? This action cannot be undone.</p>' +
+            '</div>' +
+            '<div class="modal-footer">' +
+              '<a data-dismiss="modal" class="btn btn-success">Cancel</a>' +
+              '<a data-dismiss="modal" class="btn btn-danger confirm">Delete account</a>' +
+            '</div>' +
+          '</div>' +
+        '</div>' +
+      '</div>';
+      
+    $(html).modal();
+    $('#confirmationDialog .confirm').on('click', function() {
+      $.rails.confirmed(link);
+    });
+  };
+
+  $.rails.allowAction = function(link) {
+    if (!link.attr('data-confirm')) {
+      return true;
+    }
+    
+    $.rails.showConfirmDialog(link);
+    
+    return false;
+  };
+});
